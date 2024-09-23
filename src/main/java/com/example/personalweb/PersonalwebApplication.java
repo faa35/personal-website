2509640eff8555f685bd901dcc47d2dda0494a02
Fardin <faa35@sfu.ca>
2024-09-22 23:26:28 -0700

    Initial commit added files for the main page

---
+++ src/main/java/com/example/personalweb/PersonalwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalwebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/personalweb/controllers/mainPageController.java
@@ -0,0 +1,20 @@
+package com.example.personalweb.controllers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainPageController {
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("title", "Welcome to My Personal Website");
+        return "mainPage"; 
+    }
+
+
+    
+}
